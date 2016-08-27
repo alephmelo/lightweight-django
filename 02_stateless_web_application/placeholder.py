20aa8045f9f1af9ceffe51c3b5b972040f5a1633
Aleph Melo <alephmelo@icloud.com>
2016-08-26 23:48:49 -0300

    Generate method to form class.

--- 02_stateless_web_application/placeholder.py
+++ 02_stateless_web_application/placeholder.py
@@ -34,6 +34,15 @@ class ImageForm(forms.Form):
     height = forms.IntegerField(min_value=1, max_value=2000)
     width = forms.IntegerField(min_value=1, max_value=2000)
 
+    def generate(self, image_format='PNG'):
+        """Generate an image of the given type and return as raw bytes."""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        image = Image.new('RGB', (width, height))
+        content = BytesIO()
+        image.save(content, image_format)
+        content.seek(0)
+        return content
 
 
 def index(request):
